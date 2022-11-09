d8ffa71cbc0b8a7476deabcc596b88b4d61d69c2
hrt0809 <hrt.ueda0809@gmail.com>
2022-11-09 13:46:06 +0900

    update: チーム数4ラウンド数3となる組合せ全テスト

--- tests/test_byes.py
+++ tests/test_byes.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from optbyes.optbyes import OptimizeByes
-from optbyes.problem import BaseProblem, Problem
+from optbyes.problem import BaseProblem, BaseProblemFactory, Problem, ProblemFactory
 from optbyes.utils import TeamSequence
 
 
@@ -49,3 +49,20 @@ def test_byes_teams4_rounds6_2() -> None:
     assert p1.get_status() == Problem.INFEASIBLE
     assert p2.get_status() == Problem.OPTIMAL
     assert p2.get_num_byes() == 12
+
+
+def test_byes_teams4_round3() -> None:
+    ss = [
+        {1: (2, 3, 4), 2: (1, 4, 3), 3: (4, 1, 2), 4: (3, 2, 1)},
+        {1: (2, 4, 3), 2: (1, 3, 4), 3: (4, 2, 1), 4: (3, 1, 2)},
+        {1: (3, 2, 4), 2: (4, 1, 3), 3: (1, 4, 2), 4: (2, 3, 1)},
+        {1: (3, 4, 2), 2: (4, 3, 1), 3: (1, 2, 4), 4: (2, 1, 3)},
+        {1: (4, 2, 3), 2: (3, 1, 4), 3: (2, 4, 1), 4: (1, 3, 2)},
+        {1: (4, 3, 2), 2: (3, 4, 1), 3: (2, 1, 4), 4: (1, 2, 3)},
+    ]
+    for s in ss:
+        factory: ProblemFactory = BaseProblemFactory()
+        p = OptimizeByes(s, factory)
+        p.solve()
+        assert p.get_status() == Problem.OPTIMAL
+        assert p.get_num_rounds() == 3
