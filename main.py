81d5724d37839a6623764879329f69998f5bb499
hrt0809 <hrt.ueda0809@gmail.com>
2022-11-20 20:48:28 +0900

    add: the feature of drawing graphs.

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from optbyes import utils
-from optbyes._typing import TeamPriority
-from optbyes.optbyes import OptimizeNumberOfByes
-from optbyes.problem.factory import BaseProblemFactory, MinimizeConsecutiveByesProblemFactory, ProblemFactory
+import time
+
+import optbyes as opb
+from optbyes.utils import converter
 
 
-def main(team_priority: TeamPriority, prob_type: str = "base") -> None:
-    problem_factories = {"base": BaseProblemFactory(), "min_consec": MinimizeConsecutiveByesProblemFactory()}
-    problem_factory: ProblemFactory = problem_factories[prob_type]
-    ob = OptimizeNumberOfByes(team_priority, problem_factory)
-    ob.solve()
-    if ob.get_status() == OptimizeNumberOfByes.OPTIMAL:
-        ob.print_schedule()
+def main(team_priority: opb.TeamPriority) -> None:
+    # solve Graph
+    solver_graph = opb.BaseProbSolverWithGraph.create_from_team_priority(team_priority)
+    # solve ILP
+    factory = opb.BaseILPFactory()
+    solver_ilp = opb.ProbSolverWithGurobi.create_from_team_priority(team_priority, factory)
 
+    # Graph Algorithm vs Gurobi
+    calc_times: dict[str, float] = {}
+    solvers: dict[str, opb.OptByesSolver] = {"graph": solver_graph, "gurobi": solver_ilp}
+    for solver_name, solver in solvers.items():
+        start_time = time.perf_counter()
+        solver.solve()
+        calc_time = time.perf_counter() - start_time
+        calc_times[solver_name] = calc_time
+        if solver.get_status() == opb.OPTIMAL:
+            solver.print_schedule()
+    print(calc_times)
 
-def check_all_combinations(num_teams: int = 4, num_fixed: int = 0) -> None:
-    from scipy import special
-
-    max_round = int(special.comb(num_teams, 2, exact=True))
-    feasible_sols: dict[int, list[TeamPriority]] = {i: [] for i in range(num_teams - 1, max_round + 1)}
-    team_priorities: list[TeamPriority] = utils.generate_team_priorities(num_teams, num_fixed)
-    for tp in team_priorities:
-        factory = BaseProblemFactory()
-        ob = OptimizeNumberOfByes(tp, factory)
-        ob.solve()
-        if ob.get_status() == OptimizeNumberOfByes.OPTIMAL:
-            feasible_sols[ob.get_num_rounds()].append(tp)
-
-    all_combs = len(team_priorities)
-    feasible_combs = sum(len(feasible_sols[i]) for i in range(num_teams - 1, max_round + 1))
-    print(f"{all_combs = }, {feasible_combs = }")
+    # Draw graph and simulate schedule
+    G = converter.convert_team_priority_to_graph(team_priority)
+    opb.draw_simulation("./figures/sample.gif", G, interval=1200)
 
 
 if __name__ == "__main__":
-    tp: TeamPriority = {1: (2, 3, 4), 2: (1, 4, 3), 3: (2, 1, 4), 4: (2, 3, 1)}
-    main(tp, "base")
-    check_all_combinations(num_teams=4, num_fixed=0)
+    # feasible instance
+    tp1: opb.TeamPriority = {1: (2, 3, 4), 2: (1, 3, 4), 3: (1, 2, 4), 4: (1, 2, 3)}
+    # infeasible instance
+    tp2: opb.TeamPriority = {1: (2, 3, 4), 2: (1, 3, 4), 3: (1, 4, 2), 4: (1, 2, 3)}
+    main(tp1)
